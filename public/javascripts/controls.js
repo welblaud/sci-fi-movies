8a25bdb9d372cb6ad55083f520c81c16d24b07db
Welblaud <honza.hejzl@gmail.com>
2019-01-30 10:14:47 +0100

    Layout improvements, cleaning the searchInput with JS added.

---
+++ public/javascripts/controls.js
@@ -0,0 +1,10 @@
+// controls.js
+
+'use strict';
+
+window.onload = () => {
+
+  const searchInput = document.getElementById('searchInput');
+  searchInput.addEventListener('click', () => searchInput.value = "");
+
+}
